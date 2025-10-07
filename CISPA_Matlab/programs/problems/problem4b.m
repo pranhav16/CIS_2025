ab41dc02b0b9ff4e963f5c260f5ba6478ce7683d
pranhav16 <pranhav16@gmail.com>
2025-10-06 22:34:12 -0400

    Delete CISPA_Matlab/programs/problems/problem4b.m

--- CISPA_Matlab/programs/problems/problem4b.m
+++
@@ -1,5 +0,0 @@
-% --- TODO: Problem 4b ---
-        % 1. Use, 'c_i', 'D_i', and your `point_cloud_registration` function to compute the frame F_D.
-        %
-        % 2. Display the resulting transformation.
-% --------------------------
